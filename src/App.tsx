6e2666c09b9e720970062696d15c58f3e5024319
Catalin Enache <catalinenache78@gmail.com>
2023-11-04 23:20:13 +0200

    threejs-react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import ThreeScene from './components/three-scene';
+
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+// import './App.css';
 
 function App() {
   const [cameraOn, setCameraOn] = useState(false);
-  const handleCameraChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCameraOn(event.target.checked);
-  };
-
-  useEffect(() => {
-    if (cameraOn) {
-      window.config.switchCamera();
-    } else {
-      window.config.switchCamera();
-    }
-  });
+  // const handleCameraChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setCameraOn(event.target.checked);
+  // };
+  //
+  // useEffect(() => {
+  //   if (cameraOn) {
+  //     window.config.switchCamera();
+  //   } else {
+  //     window.config.switchCamera();
+  //   }
+  // });
 
   return (
-    <div>
-      <label>
-        Switch Camera
-        <input type="checkbox" onChange={handleCameraChange} />
-      </label>
-    </div>
+    <Provider store={store}>
+      <div>
+        {/*<label>*/}
+        {/*  Switch Camera*/}
+        {/*  <input type="checkbox" onChange={handleCameraChange} />*/}
+        {/*</label>*/}
+      </div>
+      <ThreeScene />
+    </Provider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import './scenarios/basic/basic';
+// import './scenarios/basic/basic';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
